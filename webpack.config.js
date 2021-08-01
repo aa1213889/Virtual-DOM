cac79425555b5bbc343f47bc034a0cfc23c6b70e
aa1213889 <869135421@qq.com>
2021-08-02 00:36:03 +0800

    frist commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"), // 定义输出文件夹dist路径
+  },
+  devServer: {
+    contentBase: "dist",
+    compress: false,
+    port: 8080,
+  },
+};
